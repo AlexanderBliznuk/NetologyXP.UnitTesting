78437d0bbf421c950c4a9f953c87c8032b234efe
Alexander <alexander.bliznuk@yandex.ru>
2016-11-02 14:31:37 +0300

    cut spare code

--- tests/telegramTests.js
+++ tests/telegramTests.js
@@ -7,84 +7,46 @@ import assert from 'assert'
 Если у моего собеседника день рождения, месенжер предлагает отправить ему картинку с поздравлением или стикер*/
 
 
-//это, скорее, интеграционный тест?
 suite('TelegramTests', function() {
     test('ListAllContacts_LoadAllNContactsIHaveInLocalDB_GotListOfNContacts', function() {
-        let DB = new Connection(DSN);
-        let records = [
-            {name:"contact #1", /*...*/},
-            {name:"contact #N", /*...*/}
-        ];
         DB.insert(records);
 
         let contacts = Telegram.fetchAllContacts();
 
-        assert.equal(records.length, contacts);
-
-        DB.truncate();
+        assert.equal(records.length, Telegram.getContactsLength());
     });
 
-    //такое ощущение, что для данной возможности тоже нельзя написать модульный тест. А интеграционные тесты мы ещё не прходили.
-    //явно то, что у теста можетт быть несколько причин для падения. Может, что-то с базой случилось? Или с интернет-соединением?
-    test('SendMessage_ConnectedToContact_MessageHasBeenSent', function() {
-        let DB = new Connection(DSN);
-        DB.insert([
-            {name:"contact #1", /*...*/},
-        ]);
 
+    test('SendMessage_ConnectedToContact_MessageHasBeenSent', function() {
         let contact = Telegram.fetchContact(0);
-        contact.connect();
         let message = "Hi there!";
         let res = contact.sendMessage(message);
 
         assert.true(res);
         assert.equal(message, contact.getLastReceivedMessage().toString());
-
-        DB.truncate();
     });
 
     test('CreateMultiChat_RequestedContactsToJoinChatRoom_ChatRoomHasBeenCreated', function() {
-        let DB = new Connection(DSN);
-        DB.insert([
-            {name:"contact #1", /*...*/},
-            {name:"contact #2", /*...*/},
-        ]);
-
         let contact1 = Telegram.fetchContact(0),
             contact2 = Telegram.fetchContact(1);
+
         let chatRoom = Telegram.createMultiChat(contact1, contact2);
 
         assert.equal(chatRoom, Telegram.getChatRoomByID(chatRoom.getId()));
-
-        DB.truncate();
     });
 
 
     test('InviteToMultiChat_Requested3ContactsToJoinChatRoom_3ContactsAndMeAreInChatRoom', function() {
-        let DB = new Connection(DSN);
-        DB.insert([
-            {name:"contact #1", /*...*/},
-            {name:"contact #2", /*...*/},
-            {name:"contact #3", /*...*/},
-        ]);
-
         let contact1 = Telegram.fetchContact(0),
             contact2 = Telegram.fetchContact(1),
             contact3 = Telegram.fetchContact(2);
+
         let chatRoom = Telegram.createMultiChat(contact1, contact2, contact3);
 
         assert.equal(3/*contacts*/ + 1/*me*/, Telegram.getChatRoomByID(chatRoom.getId()).getParticipants().length);
-
-        DB.truncate();
     });
 
     test('SendMessageToMultiChat_ConnectedToMultiChat_MessageHasBeenSent', function() {
-        let DB = new Connection(DSN);
-        DB.insert([
-            {name:"contact #1", /*...*/},
-            {name:"contact #2", /*...*/},
-        ]);
-
         let contact1 = Telegram.fetchContact(0),
             contact2 = Telegram.fetchContact(1);
         let chatRoom = Telegram.createMultiChat(contact1, contact2, contact3);
@@ -95,6 +57,18 @@ suite('TelegramTests', function() {
         assert.equal(message, chatRoom.getLastUserMessage(Telegram.myself()).toString());
 
         DB.truncate();
+    });
+
+    test('ReceiveMessage_InitSenderAndEmitMessage_MessageHasBeenReceived', function() {
+        let sender = new Telegram.TestContact();
+        let messageSent = "Hi there!";
+        Telegram.myself().onMessageReceive = onMessageReceiveHandler;
+
+        sender.connect(Telegram.myself()).send(messageSent);
+
+        function onMessageReceiveHandler(messageGot) {
+            assert.equal(messageSent, messageGot);
+        };
     });
 
     test('ReceiveMessage_InitSenderAndEmitMessage_MessageHasBeenReceived', function() {
